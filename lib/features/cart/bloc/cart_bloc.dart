8eb0c84085aa98e3a983e24ee7d5557980472397
kasun <kasunpradeep.ds@gmail.com>
2024-07-13 13:41:51 +0530

    cart

---
+++ lib/features/cart/bloc/cart_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../../data/cart_items.dart';
+import '../../home/models/home_product_data_model.dart';
+
+part 'cart_event.dart';
+part 'cart_state.dart';
+
+class CartBloc extends Bloc<CartEvent, CartState> {
+  CartBloc() : super(CartInitial()) {
+
+    on<CartInitialEvent>(cartInitialEvent);
+    on<CartRemoveFromCartEvent>(cartRemoveFromCartEvent);
+  }
+
+  FutureOr<void> cartInitialEvent(CartInitialEvent event, Emitter<CartState> emit) {
+    emit(CartSuccessState(cartItems: cartItems));
+  }
+
+  FutureOr<void> cartRemoveFromCartEvent(CartRemoveFromCartEvent event, Emitter<CartState> emit) {
+
+    // get cart items
+
+    // check product in cart
+
+    // reu a function to remove
+    cartItems.remove(event.productDataModel);
+    // emit cart item removed
+    emit(CartSuccessState(cartItems: cartItems));
+  }
+}
