839fc4358028dfa0756b04975221025b90eb3938
kasun <kasunpradeep.ds@gmail.com>
2024-07-13 13:40:44 +0530

    wishlist

---
+++ lib/features/wishlist/bloc/wishlist_state.dart
@@ -0,0 +1,16 @@
+part of 'wishlist_bloc.dart';
+
+@immutable
+sealed class WishlistState {}
+
+abstract class WishlistActionState extends WishlistState {}
+
+final class WishlistInitial extends WishlistState {}
+
+class WishlistSuccessState extends WishlistState {
+  final List<ProductDataModel> wishlistItems;
+  WishlistSuccessState({required this.wishlistItems});
+}
+
+
+class WishlistRemovedActionState extends WishlistState {}
